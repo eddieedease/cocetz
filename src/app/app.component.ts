1704a120a44a4e17c16b080df582014bc47a19f5
Eddie Maas <eddie@edease.nl>
2020-01-10 13:36:27 +0100

    basecovered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   title = 'cocetz';
 
+  // Typescript Media querie var
+  mq = window.matchMedia('(min-width: 700px)');
 
+
+  constructor(){
+    if (this.mq.matches) {
+     
+    } else {
+      // window width is less than 500px
+      $('body').toggleClass('ms-aside-left-open');
+    }
+  }
 
   clickNav (){
-    console.log("ok")
-      $('body').toggleClass('ms-settings-open');
+    console.log("ok");
+    $('body').toggleClass('ms-aside-left-open');
   }
 
 
