6b15d3cef59ad1d00a9890eee486dc85ec5805ff
Eddie <eddie@edease.nl>
2020-02-13 22:21:47 +0100

    small update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,5 +26,37 @@ export class AppComponent  {
     $('body').toggleClass('ms-aside-left-open');
   }
 
+  // when navigating, are we on mobile => hide menu
+  checkIfMobile() {
+    const useragent = navigator.userAgent;
+
+    let isMobile = false;
+        if(useragent.match(/Android/i)) {
+          isMobile = true;
+        } else if(useragent.match(/webOS/i)) {
+          isMobile = true;
+        } else if(useragent.match(/iPhone/i)) {
+          isMobile = true;
+        } else if(useragent.match(/iPod/i)) {
+          isMobile = true;
+        } else if(useragent.match(/iPad/i)) {
+          isMobile = false;
+        } else if(useragent.match(/Windows Phone/i)) {
+          isMobile = true;
+        } else if(useragent.match(/SymbianOS/i)) {
+          isMobile = true;
+        } else if(useragent.match(/RIM/i) || useragent.match(/BB/i)) {
+          isMobile = true;
+        } else {
+            return false;
+        }
+
+    if (isMobile === true){
+      this.clickNav();
+    }
+
+  }
+
+
 
 }
